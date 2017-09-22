11147730ed45aab7a72d673cc489da65ab797e66
ChungChe Cheng <31715971+peter5500@users.noreply.github.com>
2017-09-22 11:20:04 -0700

    Update Q1Salary.java

--- assignment 2/Q1Salary.java
+++ assignment 2/Q1Salary.java
@@ -8,11 +8,9 @@ public class Q1Salary {
             return hours * 15;
         } else if (hours <= 41 && hours > 36) {
             return 36 * 15 + (hours - 36) * 15 * 1.5;
-        } else if (hours <= 48 && hours > 41) {
+        } else (hours <= 48 && hours > 41) {
             return 36 * 15 + 5 * 15 * 1.5 + (hours - 41) * 15 * 2;
-        } else {
-            return 36 * 15 + 5 * 15 * 1.5 + 7 * 15 * 2;
-        }
+        } 
     }
         public static void main(String [] args){
             Q1Salary count=new Q1Salary();
